fb503fae24617226d8d1bd434c11f21b8fcc4ea4
Abhishek Yadav <127724654+Abhi-2516@users.noreply.github.com>
2025-12-22 10:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (88.39%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string result = "";
+        string word = "";
+        
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if(s[i] != ' ') {
+                word = s[i] + word;  
+            } 
+            else if(!word.empty()) {
+                if(!result.empty()) {
+                    result += " ";
+                }
+                result += word;
+                word = "";
+            }
+        }
+        
+        if(!word.empty()) {
+            if(!result.empty()) {
+                result += " ";
+            }
+            result += word;
+        }
+        
+        return result;
+    }
+};
